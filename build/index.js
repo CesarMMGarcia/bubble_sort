e455b3d75054d9a6d491941a692897a79c1c8f7c
imhyperion <imhyperion@users.noreply.github.com>
2020-09-15 12:26:36 -0700

    sorting strings

--- build/index.js
+++ build/index.js
@@ -1,22 +1,12 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 2, 3, 1]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersColl = new NumbersCollection([10, 9, 6, 2, 5, 3]);
+// const sorter = new Sorter(numbersColl);
+// sorter.sort();
+// console.log(numbersColl.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
+var sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
+// const numbersColl = new NumbersCollection([10, 9, 6, 2, 5, 3]);
 
-const sorter = new Sorter([10, 2, 3, 1]);
+// const sorter = new Sorter(numbersColl);
 
+// sorter.sort();
+// console.log(numbersColl.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
